441edb3a962c7fcc9487870bec0256f58c2f6ef4
italoucsp <42581127+italoucsp@users.noreply.github.com>
2021-02-03 10:00:46 -0500

    Implementation of insert and split.
    Still needed "remove" method?

--- source/b-link.hpp
+++ source/b-link.hpp
@@ -7,10 +7,14 @@
 #include <condition_variable>
 #include <memory>
 #include <mutex>
+#include <queue>
 #include <stack>
 #include <stdexcept>
 #include <string>
 #include <utility>
+
+#define NODE_OVERFLOW 1
+#define STILL_SAFE 0
 
 namespace EDA {
   namespace Concurrent {
@@ -78,7 +82,13 @@ namespace EDA {
         std::mutex blink_locker;
         std::condition_variable working_on;
 
-        BLinkNode(std::size_t level = 0) : level_(level), size_(0) {}
+        BLinkNode(std::size_t level = 0) : level_(level), size_(0) {
+          high_key = std::numeric_limits<key_type>::min();
+        }
+
+        const key_type get_high_key() noexcept { return high_key; }
+
+        const std::size_t get_level() noexcept { return level_; }
 
         void lock() {
           doing_modification = true;
@@ -129,7 +139,8 @@ namespace EDA {
           std::unique_ptr<data_type> data_ptr(new data_type(data_value));
           cfield_refw.get() = std::make_shared<NodeField>(key_value, data_ptr,
                                          cfield_refw->get());
-          return (++size_ == MAX_FIELDS_X_NODE);
+          update_high_key(key_value);
+          return (++size_ > MAX_FIELDS_X_NODE);
         }
 
         bool insert(const key_type key_value, 
@@ -139,11 +150,18 @@ namespace EDA {
           PtrFieldRef cfield_refw = find_place(key_value);
           cfield_refw.get() = std::make_shared<NodeField>(key_value, next_level,
                                           cfield_refw->get());
-          return (++size_ == MAX_FIELDS_X_NODE);
+          update_high_key(key_value);
+          return (++size_ > MAX_FIELDS_X_NODE);
         }
 
         void link_with(const std::shared_ptr<BLinkNode>& node_ptr) { 
           link_pointer = node_ptr; 
+        }
+
+        void swap(std::shared_ptr<BLinkNode>& node_ptr) {
+          std::shared_ptr<NodeField> temp = head;
+          head = node_ptr->head;
+          node_ptr->head = temp;
         }
 
         std::shared_ptr<BLinkNode> scannode(const key_type key_value) {
@@ -157,6 +175,7 @@ namespace EDA {
         key_type high_key;
         std::shared_ptr<BLinkNode> link_pointer;
         std::shared_ptr<NodeField> head;
+        PtrFieldRef last_fpb = std::ref(head);
         std::size_t size_;
         std::size_t level_;
 
@@ -172,8 +191,34 @@ namespace EDA {
           return current_field_ref;
         }
 
+        void push_back(const std::shared_ptr<NodeField>& next_field) {
+          last_fpb.get() = next_field;
+          last_fpb = std::ref(last_fpb.get()->get_next_field());
+          ++size_;
+        }
+
+        void update_high_key(const key_type new_key) {
+          high_key = std::max(new_key, high_key);
+        }
+
         std::shared_ptr<BLinkNode> split() {
-          return nullptr;
+          std::shared_ptr<BLinkNode> other_half(get_level());
+          std::size_t half_size = size_ / 2;
+          size_ = size_ - size_ / 2;
+          std::queue<std::shared_ptr<NodeField>> fn_fields;
+          std::shared_ptr<NodeField> cfield = head;
+          while (half_size--) {
+            fn_fields.push(cfield);
+            cfield = cfield->get_next_field();
+          }
+          head = cfield;
+          while (!fn_fields.empty()) {
+            fn_fields.front()->set_next_field(nullptr);
+            other_half.push_back(fn_fields.front());
+            fn_fields.pop();
+          }
+          other_half.swap(*this);
+          return other_half;
         }
       };
 
@@ -226,31 +271,41 @@ namespace EDA {
         if (current->has(key_value)) {
           std::cerr << "[!]: La clave del dato a insertar ya existe en el arbol.\n";
           current->unlock();
+          current->working_on.notify_one();
           return;
         }
         std::shared_ptr<BLinkNode> oldnode = current;
+        bool tree_status = STILL_SAFE;
         //safe node
-        while (!ancestors.empty()) {
-          if (current->size() < MAX_FIELDS_X_NODE) {
-            if(current->is_leaf())
-              current->insert(key_value, data_value);
-            else
-              current->insert(key_value, );
+        while (!ancestors.empty()) {//Do insertion
+          tree_status = (current->is_leaf()) ? (current->insert(key_value, data_value)) : 
+                                    (current->insert(oldnode->get_high_key(), oldnode));
+          switch (tree_status) {
+          case STILL_SAFE:
             current->unlock();
-          }
-          else {
+            current->working_on.notify_one();
+            break;
+          case NODE_OVERFLOW:
             std::shared_ptr<BLinkNode> newnode = current->split();
             oldnode = current;
             current = ancestors.top();
-            current->insert(key_value, newnode);
+            current->insert(newnode->get_high_key(), newnode);
             ancestors.pop();
             current->lock();
-            move_right(key_value, current);
+            move_right(current->get_high_key(), current);
             oldnode->unlock();
+            oldnode->working_on.notify_one();
+            break;
           }
         }
-        current->working_on.notify_one();
-        /*...*/
+        if (tree_status == NODE_OVERFLOW) {//new root
+          root->lock();
+          std::shared_ptr<BLinkNode> newroot(root->get_level() + 1), newnode = root->split();
+          newroot->insert(root->get_high_key(), root);
+          newroot->insert(newnode->get_high_key(), newnode);
+          root->unlock();
+          root->working_on.notify_one();
+        }
       }
 
       void remove(const key_type key_value) {}
